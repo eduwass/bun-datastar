f7618e11ce8c2f7e71e8852cc716d77b4102fc24
Edu Wass <eduwass@gmail.com>
2025-04-29 17:48:01 +0200

    working sse and displaying on index file

--- index.ts
+++ index.ts
@@ -1,29 +1,34 @@
-import { serve } from "bun";
-
-serve({
+function sendSSECustom(controller: ReadableStreamDirectController, eventName: string, data: string) {
+    return controller.write(`event: ${eventName}\ndata:${JSON.stringify(data)}\n\n`);
+}
+function sendSSEMessage(controller: ReadableStreamDirectController, data: string) {
+    return controller.write(`data:${JSON.stringify(data)}\n\n`);
+}
+function sse(req: Request): Response {
+    const signal = req.signal;
+    return new Response(
+        new ReadableStream({
+            type: "direct",
+            async pull(controller: ReadableStreamDirectController) {
+                while (!signal.aborted) {
+                    await sendSSECustom(controller, "bun", "Hello, World!");
+                    await sendSSEMessage(controller, "Hello, World!");
+                    await controller.flush();
+                    await Bun.sleep(1000);
+                }
+                controller.close();
+            },
+        }),
+        { status: 200, headers: { "Content-Type": "text/event-stream" } },
+    );
+}
+Bun.serve({
     port: 3000,
     fetch(req) {
-        console.log(req);
-        if (req.url === "/events") {
-            const stream = new ReadableStream({
-                start(controller) {
-                    console.log("start");
-                    const timer = setInterval(() => {
-                        controller.enqueue(`data: ${new Date().toISOString()}\n\n`);
-                    }, 1000);
-                    controller.closed.then(() => clearInterval(timer));
-                },
-            });
-            console.log("stream", stream);
-            return new Response(stream, {
-                headers: {
-                    "Content-Type": "text/event-stream",
-                    "Cache-Control": "no-cache",
-                    "Connection": "keep-alive",
-                },
-            });
+        if (new URL(req.url).pathname === "/stream") {
+            return sse(req);
         }
-        // index.html
+        // else return index.html
         return new Response(Bun.file("index.html"));
     },
 });
