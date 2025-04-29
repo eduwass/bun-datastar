56bc97e4f7bece6b28481c9beb8942fa541aac7f
Edu Wass <eduwass@gmail.com>
2025-04-29 17:58:57 +0200

    create pkg to abstract bun-sse quirks

--- index.ts
+++ index.ts
@@ -1,34 +1,16 @@
-function sendSSECustom(controller: ReadableStreamDirectController, eventName: string, data: string) {
-    return controller.write(`event: ${eventName}\ndata:${JSON.stringify(data)}\n\n`);
-}
-function sendSSEMessage(controller: ReadableStreamDirectController, data: string) {
-    return controller.write(`data:${JSON.stringify(data)}\n\n`);
-}
-function sse(req: Request): Response {
-    const signal = req.signal;
-    return new Response(
-        new ReadableStream({
-            type: "direct",
-            async pull(controller: ReadableStreamDirectController) {
-                while (!signal.aborted) {
-                    await sendSSECustom(controller, "bun", "Hello, World!");
-                    await sendSSEMessage(controller, "Hello, World!");
-                    await controller.flush();
-                    await Bun.sleep(1000);
-                }
-                controller.close();
-            },
-        }),
-        { status: 200, headers: { "Content-Type": "text/event-stream" } },
-    );
-}
+import { createSSEHandler } from "bun-sse";
+
+const sse = createSSEHandler(async (stream, req) => {
+  while (!req.signal.aborted) {
+    stream.event("bun", { hello: "world" });
+    stream.send("Hello!");
+    await Bun.sleep(1000);
+  }
+});
+
 Bun.serve({
-    port: 3000,
-    fetch(req) {
-        if (new URL(req.url).pathname === "/stream") {
-            return sse(req);
-        }
-        // else return index.html
-        return new Response(Bun.file("index.html"));
-    },
+  fetch(req) {
+    if (new URL(req.url).pathname === "/stream") return sse(req);
+    return new Response(Bun.file("index.html"));
+  },
 });
